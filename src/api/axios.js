5fb1c71d79caa3159b12aae1461c16febc4728b6
Sachin Bind <sachinbind7977@gmail.com>
2026-06-07 01:28:26 +0530

    add live api link

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL:'https://debubattle2bakend.onrender.com' || 'http://localhost:5000/api',
   withCredentials: true, // Required to send cookies
 });
 
